b5bb84bb16c447caec074903469d7399ccf87585
Lars Söderkvist <updates11@mac.com>
2013-12-17 15:20:30 -0600

    Add test for names entered in uppercase.

--- test/test_cheers_integration.rb
+++ test/test_cheers_integration.rb
@@ -2,6 +2,18 @@ require_relative 'helper'
 
 class TestCheersIntegration < MiniTest::Unit::TestCase
 
+    # - - - - - - - characters that take 'a' before - - - - - - - #
+
+  def test_a_name_with_only_characters_that_take_a_before_in_correct_order
+    shell_output = ""
+    IO.popen('ruby cheers.rb', 'r+') do |pipe|
+      pipe.puts("duck")
+      pipe.close_write
+      shell_output = pipe.read
+    end
+    assert_includes_in_order shell_output, "Give me a... D", "Give me a... U", "Give me a... C", "Give me a... K"
+  end
+
   def test_a_name_with_only_characters_that_take_a_before
     shell_output = ""
     IO.popen('ruby cheers.rb', 'r+') do |pipe|
@@ -20,15 +32,7 @@ EOS
     assert_equal expected_output, shell_output
   end
 
-  def test_a_name_with_only_characters_that_take_a_before_a_different_way
-    shell_output = ""
-    IO.popen('ruby cheers.rb', 'r+') do |pipe|
-      pipe.puts("duck")
-      pipe.close_write
-      shell_output = pipe.read
-    end
-    assert_includes_in_order shell_output, "Give me a... D", "Give me a... U", "Give me a... C", "Give me a... K"
-  end
+    # - - - - - - - characters that take 'an' before - - - - - - - #
 
   def test_a_name_with_only_characters_that_take_an_before
     shell_output = ""
@@ -48,6 +52,8 @@ EOS
     assert_equal expected_output, shell_output
   end
 
+    # - - - - - - - characters that take both 'a' and 'an' before - - - - - - - #
+
   def test_a_name_with_characters_that_take_both_a_and_an
     shell_output = ""
     IO.popen('ruby cheers.rb', 'r+') do |pipe|
@@ -64,6 +70,8 @@ BOB's just GRAND!
 EOS
     assert_equal expected_output, shell_output
   end
+
+    # - - - - - - - ending characters - - - - - - - #
 
   def test_a_name_ending_in_s
     shell_output = ""
@@ -93,6 +101,25 @@ EOS
       shell_output = pipe.read
     end
     assert_includes shell_output, "FELIX is just GRAND!"
+  end
+
+    # - - - - - - - uppercase characters - - - - - - - #
+
+  def test_a_name_entered_as_uppercase_characters
+    shell_output = ""
+    IO.popen('ruby cheers.rb', 'r+') do |pipe|
+      pipe.puts("BOB")
+      pipe.close_write
+      shell_output = pipe.read
+    end
+    expected_output = <<EOS
+What's your name?
+Give me a... B
+Give me an... O
+Give me a... B
+BOB's just GRAND!
+EOS
+    assert_equal expected_output, shell_output
   end
 
 end
